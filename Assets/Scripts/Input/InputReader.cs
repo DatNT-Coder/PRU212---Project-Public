c9413ef501959b0c6dc656c111deeac84b7a0d9d
khangns176045 <khangnshe176045@fpt.edu.vn>
2024-10-06 14:13:07 +0700

    Implement networked player movement and aiming mechanics
    Implement  networked player movement and aiming systems

--- Assets/Scripts/Input/InputReader.cs
+++ Assets/Scripts/Input/InputReader.cs
@@ -8,35 +8,42 @@ using static Controls;
 [CreateAssetMenu(fileName = "New Input Reader", menuName = "Input/Input Reader")]
 public class InputReader : ScriptableObject, IPlayerActions
 {
-	public event Action<bool> PrimaryFireEvent;
-	public event Action<Vector2> MoveEvent;
+    public event Action<bool> PrimaryFireEvent;
+    public event Action<Vector2> MoveEvent;
 
-	private Controls controls;
-	private void OnEnable()
-	{
-		if (controls == null)
-		{
-			controls = new Controls();
-			controls.Player.SetCallbacks(this);
-		}
+    public Vector2 AimPosition { get; private set; }
 
-		controls.Player.Enable();
-	}
-	public void OnMove(InputAction.CallbackContext context)
-	{
-		MoveEvent?.Invoke(context.ReadValue<Vector2>());
-	}
+    private Controls controls;
+    private void OnEnable()
+    {
+        if (controls == null)
+        {
+            controls = new Controls();
+            controls.Player.SetCallbacks(this);
+        }
 
-	public void OnPrimaryFire(InputAction.CallbackContext context)
-	{
-		if (context.performed)
-		{
-			PrimaryFireEvent.Invoke(true);
-		}
-		else if (context.canceled)
-		{
-			PrimaryFireEvent.Invoke(false);
-		}
+        controls.Player.Enable();
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        MoveEvent?.Invoke(context.ReadValue<Vector2>());
+    }
 
-	}
+    public void OnPrimaryFire(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            PrimaryFireEvent.Invoke(true);
+        }
+        else if (context.canceled)
+        {
+            PrimaryFireEvent.Invoke(false);
+        }
+
+    }
+
+    public void OnAim(InputAction.CallbackContext context)
+    {
+        AimPosition = context.ReadValue<Vector2>();
+    }
 }
