2799d35c26525e277a733903360bf27b7194126a
Luu Duong Bach <LuuBach.it@gmail.com>
2024-10-14 10:34:31 +0700

    Creating Lobbies
    Creating Lobbies

--- Assets/Scripts/Networking/Host/HostGameManager.cs
+++ Assets/Scripts/Networking/Host/HostGameManager.cs
@@ -5,39 +5,36 @@ using System.Threading.Tasks;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using Unity.Networking.Transport.Relay;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class HostGameManager
-{
+public class HostGameManager {
 
     private Allocation allocation;
     private string joinCode;
+    private string lobbyId;
 
     private const int MaxConnections = 20;
     private const string GameSceneName = "Game";
 
-    public async Task StartHostAsync()
-    {
-        try
-        {
+    public async Task StartHostAsync() {
+        try {
             allocation = await Relay.Instance.CreateAllocationAsync(MaxConnections);
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             Debug.Log(e);
             return;
         }
 
-        try
-        {
+        try {
             joinCode = await Relay.Instance.GetJoinCodeAsync(allocation.AllocationId);
             Debug.Log(joinCode);
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             Debug.Log(e);
             return;
         }
@@ -47,9 +44,43 @@ public class HostGameManager
         RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
         transport.SetRelayServerData(relayServerData);
 
+        try {
+            CreateLobbyOptions lobbyOptions = new CreateLobbyOptions();
+            lobbyOptions.IsPrivate = false;
+            lobbyOptions.Data = new Dictionary<string, DataObject>()
+            {
+                {
+                    "JoinCode", new DataObject(
+                        visibility: DataObject.VisibilityOptions.Member,
+                        value: joinCode
+                    )
+                }
+            };
+            Lobby lobby = await Lobbies.Instance.CreateLobbyAsync(
+                "My Lobby", MaxConnections, lobbyOptions);
+
+            lobbyId = lobby.Id;
+
+            HostSingleton.Instance.StartCoroutine(HearbeatLobby(15));
+        }
+        catch (LobbyServiceException e) {
+            Debug.Log(e);
+            return;
+        }
+
+
         NetworkManager.Singleton.StartHost();
 
         NetworkManager.Singleton.SceneManager.LoadScene(GameSceneName, LoadSceneMode.Single);
     }
+
+    private IEnumerator HearbeatLobby(float waitTimeSeconds) {
+        WaitForSecondsRealtime delay = new WaitForSecondsRealtime(waitTimeSeconds);
+        while (true) {
+            Lobbies.Instance.SendHeartbeatPingAsync(lobbyId);
+            yield return delay;
+        }
+    }
+
 
 }
