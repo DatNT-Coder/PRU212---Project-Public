b15b49b9f4afdd566440c09ae92fce31f1cada36
Dao Tan Phat <daotanphat2143@gmail.com>
2024-10-19 10:27:46 +0700

    feat: add leaderboard and sorting the order

---
+++ Assets/Scripts/UI/Leaderboard/LeaderboardEntityState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Collections;
+using Unity.Netcode;
+
+namespace Assets.Scripts.UI.Leaderboard
+{
+	public struct LeaderboardEntityState : INetworkSerializable, IEquatable<LeaderboardEntityState>
+	{
+		public ulong ClientId;
+		public FixedString32Bytes PlayerName;
+		public int Coins;
+
+		public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+		{
+			serializer.SerializeValue(ref ClientId);
+			serializer.SerializeValue(ref PlayerName);
+			serializer.SerializeValue(ref Coins);
+		}
+
+		public bool Equals(LeaderboardEntityState other)
+		{
+			return ClientId == other.ClientId &&
+				PlayerName.Equals(other.PlayerName) &&
+				Coins == other.Coins;
+		}
+	}
+}
